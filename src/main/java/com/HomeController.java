1708bf3801a3712ecd8bc2372f3fc99894736614
BorsosIstvan <bistvanus@gmail.com>
2021-12-05 16:34:36 +0100

    werk werk

--- src/main/java/com/HomeController.java
+++ src/main/java/com/HomeController.java
@@ -1,12 +1,26 @@
 package com;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 	@RequestMapping("/")
 	public String viewHomePage() {
+		return "index";
+	}
+	
+	@GetMapping("/contacts")
+	public String listContacts() {
+		return "index";
+	}
+	@GetMapping("/users")
+	public String listUsers() {
+		return "index";
+	}
+	@GetMapping("/logout")
+	public String loggingOut() {
 		return "index";
 	}
 
