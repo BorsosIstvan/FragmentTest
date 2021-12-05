3189603a7bda68a8cdd62191782614d469264864
BorsosIstvan <bistvanus@gmail.com>
2021-12-05 15:36:53 +0100

    files uploaded

---
+++ src/main/java/com/FragmentTestApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FragmentTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FragmentTestApplication.class, args);
+	}
+
+}
